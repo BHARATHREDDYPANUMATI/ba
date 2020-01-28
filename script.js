76eea55bffc6b0d37809322d8944c1a376e52406
BHARATHREDDYPANUMATI <49202214+BHARATHREDDYPANUMATI@users.noreply.github.com>
2020-01-28 11:43:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                 lat:17.456199,
+                lng: 78.666740,
             },
         },
     ];
